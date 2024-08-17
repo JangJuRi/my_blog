b519ae0c4e06a1ebea2532f6605448fb3d33c9fd
장주리 <wkdwnfl119@naver.com>
2024-08-17 16:28:11 +0900

    ALLOWED_HOSTS 수정

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-0bgb%=grjfxl*e=eoqfp%ai-nspzc%la%5podf&q+avm2awa!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
